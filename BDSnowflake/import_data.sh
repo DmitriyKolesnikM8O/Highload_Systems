1f90a92f67fae26c89308fa1f3781e3ab79e754b
Dmitriy <kolesnik982011@gmail.com>
2026-03-14 22:31:45 +0300

    delete some lines

--- BDSnowflake/import_data.sh
+++ BDSnowflake/import_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "🚀 Создаем таблицу и загружаем первый файл..."
+echo "Создаем таблицу и загружаем первый файл..."
 csvsql --db postgresql://postgres:postgres@localhost:5432/postgres \
   --insert --overwrite --tables mock_data -d "," "./data/MOCK_DATA.csv"
 
 for i in {1..9}; do
-    echo "📥 Загрузка файла ($i)..."
+    echo "Загрузка файла ($i)..."
     csvsql --db postgresql://postgres:postgres@localhost:5432/postgres \
       --insert --no-create --tables mock_data -d "," "./data/MOCK_DATA ($i).csv"
 done
--- BDSnowflake/init/02_snowflake.sql
+++ BDSnowflake/init/02_snowflake.sql
@@ -1,4 +1,3 @@
--- 1. Сначала создаем чистые измерения (только уникальные значения)
 CREATE TABLE dim_suppliers AS
 SELECT DISTINCT supplier_name, supplier_contact, supplier_email, supplier_phone, supplier_address, supplier_city, supplier_country
 FROM mock_data;
@@ -19,7 +18,6 @@ SELECT DISTINCT store_name, store_location, store_city, store_state, store_count
 FROM mock_data;
 ALTER TABLE dim_stores ADD COLUMN store_id SERIAL PRIMARY KEY;
 
--- 2. Снежинка: Товары (привязываем к ID поставщика, используя LIMIT 1 для страховки от дублей)
 CREATE TABLE dim_products AS
 SELECT DISTINCT ON (m.product_name) 
     m.product_name, m.product_category, m.product_brand, m.product_price, s.supplier_id
@@ -27,7 +25,6 @@ FROM mock_data m
 JOIN dim_suppliers s ON m.supplier_name = s.supplier_name;
 ALTER TABLE dim_products ADD COLUMN product_id SERIAL PRIMARY KEY;
 
--- 3. Снежинка: Питомцы (привязываем к ID клиента)
 CREATE TABLE dim_pets AS
 SELECT DISTINCT ON (m.customer_pet_name, m.customer_email)
     m.customer_pet_name, m.customer_pet_type, m.customer_pet_breed, c.customer_id
@@ -35,7 +32,6 @@ FROM mock_data m
 JOIN dim_customers c ON m.customer_email = c.customer_email;
 ALTER TABLE dim_pets ADD COLUMN pet_id SERIAL PRIMARY KEY;
 
--- 4. ТАБЛИЦА ФАКТОВ (теперь делаем JOIN максимально аккуратно)
 CREATE TABLE fact_sales AS
 SELECT 
     m.sale_date,
